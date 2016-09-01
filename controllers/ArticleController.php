87c3147fdd5e05d6da27b84219a6353bbdb905c1
Lid <nwps8716@gmail.com>
2016-09-01 07:53:51 +0000

    增加htmlentities()

--- controllers/ArticleController.php
+++ controllers/ArticleController.php
@@ -1,22 +1,22 @@
 <?php
 class ArticleController extends Controller {
-    
+
     //上傳文章
     function upload() {
         $this->model("CRUD");
         $crud = new CRUD();
-        
+
         if(isset($_POST['uploadcontent'])) {
             date_default_timezone_set('Asia/Taipei');  //Cloud9上面的時間是格林威治的時間
             $date = date("Y.m.d");
-            
-            $title = $_POST["uploadtitle"];
-        	$content = $_POST["uploadcontent"];
+
+            $title = htmlentities($_POST["uploadtitle"]);
+        	$content = htmlentities($_POST["uploadcontent"]);
         	$user = $_POST['userid'];
-        	
+
         	$picture = $_FILES["uploadfileimage"]["name"];
         	move_uploaded_file($_FILES["uploadfileimage"]["tmp_name"],"views/img/".$_FILES["uploadfileimage"]["name"]);
-        	
+
         	$row = $crud->pdouploadArticle($title,$content,$user,$picture,$date);
         	if($row){
         	    header("Location:/Project_Good_Eat_PDO/Home/blog");
@@ -25,13 +25,13 @@ class ArticleController extends Controller {
         }
         $this->view("upload");
     }
-    
+
     //修改文章
     function modify() {
         $this->model("CRUD");
         $crud = new CRUD();
         $modifyArray = Array();
-        
+
         $modifyArray["id"][0] = $_GET['id'];
         $modifyArray["img"][0] = $_GET['img'];
         $modifyArray["tit"][0] = $_GET['tit'];
@@ -40,10 +40,10 @@ class ArticleController extends Controller {
         if(isset($_POST['title'])) {
             $picture = $_FILES["fileimage"]["name"];
         	move_uploaded_file($_FILES["fileimage"]["tmp_name"],"views/img/".$_FILES["fileimage"]["name"]);
-        	
+
             $id = $_POST["id"];
-        	$title = $_POST["title"];
-        	$content = $_POST["content"];
+        	$title = htmlentities($_POST["title"]);
+        	$content = htmlentities($_POST["content"]);
 
         	if($picture == ""){
         	    $row = $crud->pdoupdateArticle($title,$content,$id);
@@ -57,12 +57,12 @@ class ArticleController extends Controller {
         }
         $this->view("modify",$modifyArray);
     }
-    
+
     //刪除文章、圖片、留言
     function del() {
         $this->model("CRUD");
         $crud = new CRUD();
-        
+
         if(isset($_GET['id']))
         {
             $id = $_GET['id'];
@@ -70,39 +70,39 @@ class ArticleController extends Controller {
             if (file_exists("views/img/".$img)) {
                 unlink("views/img/".$img);
             }
-            
+
             $row = $crud ->pdodeleteArticle($id,$img);
             $message = $crud ->pdodeleteMessageboard($id);
-            
+
             if(isset($row) && isset($message)) {
                 header("Location:/Project_Good_Eat_PDO/Home/selfpost");
                 exit;
             }
         }
     }
-    
+
     //上傳留言
-    function uploadmessage() { 
+    function uploadmessage() {
         $this->model("CRUD");
         $crud = new CRUD();
-        
+
         if(isset($_POST['message'])) {
             date_default_timezone_set('Asia/Taipei');  //Cloud9上面的時間是格林威治的時間
             $date = date("Y.m.d H:i:s");
-            
+
             $ID = $_POST["blogID"];
             $userID = $_POST["userid"];
-        	$content = $_POST["message"];
-        	
+        	$content = htmlentities($_POST["message"]);
+
         	$row = $crud ->pdouploadMessageboard($ID,$userID,$content,$date);
-        	
+
         	if($row){
         	    header("Location:/Project_Good_Eat_PDO/Home/blog");
-                exit; 
+                exit;
         	}
             $this->blog();
         }
     }
-    
+
 }
 ?>
